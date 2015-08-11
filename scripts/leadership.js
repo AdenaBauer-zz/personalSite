e055479da75e06a1ca17345cbbae922cf9d4ffa0
AdenaBauer <adena32@gmail.com>
2015-08-10 18:17:38 -0700

    adding code to git

---
+++ scripts/leadership.js
@@ -0,0 +1,23 @@
+
+var leadership = [
+    {
+        //Spark
+        "company": "<a href='http://www.sparksc.org' target='_blank'>Spark SC</a>: USC Tech and Entrepreneurship Organization",
+        "job": "Director of Operations, Finance :: April 2014 to Present",
+        "descrip": "<ul><li>One of 6 founders of Spark SC</li><li>Promoted from Director of Communications after 6 months</li><li>Planed, executed and facilitated all Spark SC events, approximatly 50 events per year, spanning from 30 to 800 in attendance</li><li>Designed financial tracking process for Spark SC which controls approximatley $50,000 moving in and out of 4 different accounts</li><li>Managed relationship with USC funding boards and administration</li><li>Delegated tasks and responsibilites to Spark SC's growing team members</li><li>Weekly commitment of 25hrs/week</li></ul>"
+
+    },
+    {
+        //HackSC
+        "company": "HackSC: USC's Primere, 48 hour Hackathon",
+        "job": "Treasurer and Operations :: March 2015 to Present",
+        "descrip": "<ul><li>Coordinated USC's 800-person Hackathon with a team of 7 students</li><li>Managed invoicing, billing and collecting hundreds of thousands of dollars from the world's top tech companies and event sponsors</li><li>Created HackSC's budget and oversaw all spending</li><li>Headed all scheduling and booking towards travel, food, space etc for the event and its attendees</li><li>Worked with connections in the USC adminsitration to win endorsements for HackSC</li></ul>"
+
+    },
+    {
+        //GSA
+        "company": "Google",
+        "job": "Google Student Ambassador, USC :: June 2015 to Present",
+        "descrip": "<ul><li>Sole student representative for Google at USC</li><li>Helped organize Google's recruiting events at USC</li><li>Acted as the liason between USC' engineering/business talent and Google </li><li>Offered insights about on-campus events that would benefit with Google's participation</li></ul>"
+    }
+]
---
+++ scripts/work.js
@@ -0,0 +1,24 @@
+
+var work = [
+    {
+        //Nordstrom
+        "company": "Nordstrom, Inc.",
+        "job": "Software Engineering Intern, Nordstrom Headquarters :: June 2015 to September 2015",
+        "descrip": "<ul><li>Worked on Nordstrom's Livesite Operations and Engineering Team (LEO) to ensure the relaibility of Nordstrom.com</li><li>Furthered the transistion of Nordstrom.com from on-premise to cloud servers by building and testing new AWS pull servers</li><li>Built configuration scripts in PowerShell for the Fullfilment ... IDK FTS UHHH</li><li>Documented the process of building and load balancing AWS instances so they are configured to handle Nordstrom.com requests</li><li>Restructured the DSC directories for Nordstrom.com, making our configuration files more modular and manipulatabe</li></ul>"
+
+    },
+    {
+        //AppNexus
+         "company": "Spark SC: USC Tech and Entrepreneurship Organization",
+        "job": "Director of Operations, Finance :: April 2014 to Present",
+        "descrip": "<ul><li>Promoted from Director of Communications after 6 months</li><li>Planed, executed and facilitated all Spark SC events, approximatly 50 events per year, spanning from 30 to 800 in attendance</li><li>Designed financial tracking process for Spark SC which controls approximatley $50,000 moving in and out of 4 different accounts</li><li>Managed relationship with USC funding boards and administration</li><li>Weekly commitment of 25hrs/week</li></ul>"
+
+    },
+    {
+        //Blackstone
+        "company": "The Blackstone Group: Blackstone Launchpad, USC",
+        "job": "Communications and Event Coordination :: August 2014 to Present",
+        "descrip": "<ul><li>Scheduled and coordinated student events, tech workshops and speaker events</li><li>Encouraged students to participate in Blackstone Venture Coaching, acting as a lesion between students and coaches</li><li>Designed and wrote the weekly newsletter</li><br><br></ul>"
+
+    }
+]
